d882834ec1ba7a34cca63ed4b0d9e81569960889
Sebastián <sebastian.salas.0999@gmail.com>
2025-04-10 11:59:31 -0500

    EJER1

---
+++ SEMANA 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejercicio01();
+            Console.ReadKey();
+        }
+
+        static void Ejercicio01()
+        {
+            string nombre;
+            Console.Write("Introduzca un nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("\n" + nombre+ " Bienvenido al curso de Fundamentos de Algoritmos");
+        }
+    }
+}
