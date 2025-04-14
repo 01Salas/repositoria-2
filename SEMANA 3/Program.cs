38956b08a87c57e582c15196998a938aed210d64
Sebastián <sebastian.salas.0999@gmail.com>
2025-04-14 13:21:46 -0500

    ejercicios pendientes

--- SEMANA 3/Program.cs
+++ SEMANA 3/Program.cs
@@ -10,7 +10,7 @@ namespace SEMANA_3
     {
         static void Main(string[] args)
         {
-            Ejercicio01();
+            Ejercicio08();
             Console.ReadKey();
         }
 
@@ -22,5 +22,87 @@ namespace SEMANA_3
 
             Console.WriteLine("\n" + nombre+ " Bienvenido al curso de Fundamentos de Algoritmos");
         }
+
+        static void Ejercicio02()
+        {
+            Console.WriteLine("Sebastian\n19\nIng.Sistemas");
+        }
+
+        static void Ejercicio03()
+        {
+
+            Console.WriteLine("\"Sebastián\"");
+
+        }
+
+        static void Ejercicio04()
+        {
+
+            Console.WriteLine();
+
+        }
+
+        static void Ejercicio05()
+        {
+
+            int bs, alt;
+            double area;
+            Console.WriteLine("Ingrese la base del triangulo");
+            bs = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la altura del triangulo: ");
+            alt = int.Parse(Console.ReadLine());
+
+            area = (bs * alt) / 2;
+
+            Console.WriteLine("\nEl area del triangulo es: " + area);
+
+        }
+
+        static void Ejercicio06()
+        {
+
+            double num1, num2, num3, promedio;
+
+            Console.Write("Ingrese el primer numero: ");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo numero: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el tercer numero: ");
+            num3 = double.Parse(Console.ReadLine());
+
+            promedio = (num1 + num2 + num3) / 3;
+
+            Console.WriteLine("\nEl promedio es: " + Math.Round(promedio, 2));
+
+
+        }
+
+        static void Ejercicio07()
+        {
+            int radio;
+            double area;
+
+            Console.Write("Ingrese el radio del circulo: ");
+            radio = Convert.ToInt32(Console.ReadLine());
+
+            area = Math.PI * Math.Pow(radio, 2);
+
+            Console.WriteLine("\nEl area del circulo es: " + Math.Round(area, 2));
+        }
+
+        static void Ejercicio08()
+        {
+            double num1;
+
+            Console.Write("Ingrese un numero decimal");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nLa raíz cuadrad es: " +Math.Sqrt(num1));
+            Console.WriteLine("La potencia a 3 es: " + Math.Pow(num1, 3));
+        }
+
     }
 }
