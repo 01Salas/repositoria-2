a2f503c66d13223521e9ee8b66a0194e7ffdc58f
Sebas01 <sebastian.salas.0999@gmail.com>
2025-06-26 12:22:17 -0500

    Semana 14 teoría

---
+++ TEORÍA/EJERCICIO 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cad = "Lenguaje Programación Java C#";
+            //Método split(). Convierte una cadena string a arreglo
+            //Debe existir un separador de palabras que se indica como parámetro
+            string[] arreglo = cad.Split(' ');
+
+            Console.WriteLine("Palabra 1: " + arreglo[0]);
+            Console.WriteLine("Palabra 2: " + arreglo[1]);
+            Console.WriteLine("Palabra 3: " + arreglo[2]);
+            Console.WriteLine("Palabra 4: " + arreglo[3]);
+
+            Console.WriteLine("Longitud: " + cad.Length);
+            Console.WriteLine("Primera Letra: " + cad[0]);
+            Console.WriteLine("Ultima letra: " + cad[cad.Length-1]);
+            Console.WriteLine("pos Primer espacio: " + cad.IndexOf(' '));
+            Console.WriteLine("Primera palabra: " + arreglo[0]);
+            Console.WriteLine("Última palabra: " + arreglo[3]);
+
+            //Elabore una función que reciba una cadena y retorne la cantidad de vocales de la cadena.
+            //Elabore una función que reciba una cadena y calcule la cantidad de vocales, consonantes,
+            //digitos, simbolos especiales de la cadena.
+
+            int con = 0;
+            for (int i = 0; i < cad.Length; i++)
+            {
+                if (cad[i] == 'a')
+                    con++;
+            }
+            Console.WriteLine("Vocales a: " + con);
+        }
+    }
+}
---
+++ TEORÍA/SEMANA 14/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombres = "Carlos Carbajal";
+            //Obtenemos la longitud de la cadena
+            int longitud = nombres.Length;
+            Console.WriteLine($"Longitud: {longitud}");
+
+            int pos = nombres.IndexOf("C");
+            Console.WriteLine($"Posicion: {pos}");
+
+            string nombreMayus = nombres.ToUpper();
+            Console.WriteLine($"Mayúscula: {nombreMayus}");
+
+            string nombreMinus = nombres.ToLower();
+            Console.WriteLine($"Minúsculas: {nombreMinus}");
+
+            string nombre1 = nombres.Substring(7);
+            Console.WriteLine($"Nombres: {nombre1}");
+
+            //Extrae una subcadena (6 caracteres) desde la posición 0
+             string nombre2 = nombres.Substring(0,6);
+             Console.WriteLine($"NOmbres: {nombre2}");
+
+             //string nombres = "Carlos Carbajal"
+
+             int pos1 = nombres.IndexOf("C",1);
+             Console.WriteLine($"Posición 2: {pos1}");
+
+            //Extraemos 1 caracter especificando su posición
+            Console.WriteLine($"Caracter: {nombres[5]}");
+
+            //De la cadena siguiente, mostrar las primeras letras de cada
+            //término
+            string estudiante = "Ana Isabel Juarez Lopez"; //Resultado=RMCM
+            string letra1, letra2, letra3, letra4;
+
+            letra1 = estudiante[0].ToString();
+            letra2 = estudiante[estudiante.IndexOf(" ") + 1].ToString();
+            letra3 = estudiante[estudiante.IndexOf(" ", estudiante.IndexOf(" ") + 1) + 1].ToString();
+            letra4 = estudiante[estudiante.IndexOf(" ", estudiante.IndexOf(" ", estudiante.IndexOf(" ") + 1) + 1) + 1].ToString();
+
+
+            Console.WriteLine($"Resultado: {letra1}{letra2}{letra3}{letra4}");
+            
+        }
+    }
+}
