6364972507efe77305732ec3e660ad84a13af373
Sebas01 <sebastian.salas.0999@gmail.com>
2025-07-10 01:59:53 -0500

    Repaso

---
+++ SebastiánEisnerSalasCueva/Pregunta01_SebastiánSalas/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta01_SebastiánSalas
+{
+    internal class Program
+    {
+        static int TotalMedallasOro, TotalMedallasPlata, TotalMedallasBronce, TotalMedallasMencionHonrosa;
+        static int canDeportistasOro, canDeportistasPlata, canDeportistasBronce, canDeportistasMencionHonrosa;
+
+        static void Main(string[] args)
+        {
+            int tipo, cantidad, punAcu;
+            string reco;
+            do
+            {
+                Console.WriteLine("Tipo de medalla: 1.Oro 2.Plata 3.Bronce 4.Mención Honrosa 5.Salir: ");
+                tipo = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Cantidad: ");
+                cantidad = int.Parse(Console.ReadLine());
+
+                punAcu = puntajeAcumulado_SebastiánSalas(tipo, cantidad);
+                reco = reconocimiento_SebastiánSalas(punAcu);
+                Incrementos(tipo, cantidad);
+                ImprimirResultados(punAcu, reco);
+                
+
+            }while (tipo!=5);
+        }
+
+        static void ImprimirResultados(int punAcu, string reco)
+        {
+            Console.WriteLine("Puntaje Acumulado: " + punAcu);
+            Console.WriteLine("Reconocimiento: " + reco);
+            Console.WriteLine("Total medallas Oro: " + TotalMedallasOro);
+            Console.WriteLine("Total medallas Plata: " + TotalMedallasPlata);
+            Console.WriteLine("Total medallas Bronce: " + TotalMedallasBronce);
+            Console.WriteLine("Total medallas Mención: " + TotalMedallasMencionHonrosa);
+
+            Console.WriteLine("Deportistas Oro: " + canDeportistasOro);
+            Console.WriteLine("Deportistas Plata: " + canDeportistasPlata);
+            Console.WriteLine("Deportistas Bronce: " + canDeportistasBronce);
+            Console.WriteLine("Deportistas  Mención: " + canDeportistasMencionHonrosa);
+
+        }
+
+        static int puntajeAcumulado_SebastiánSalas(int tipo, int cantidad)
+        {
+            int puntajeAcumulado;
+            switch (tipo)
+            {
+                case 1:
+                    puntajeAcumulado = 10 * cantidad;
+                    break;
+                case 2:
+                    puntajeAcumulado = 6 * cantidad;
+                    break;
+                case 3:
+                    puntajeAcumulado = 3 * cantidad;
+                    break;
+                default:
+                    puntajeAcumulado = 1 * cantidad;
+                    break;
+            }
+            return puntajeAcumulado;
+        }
+
+        static string reconocimiento_SebastiánSalas(int puntajeAcumulado)
+        {
+            string reconocimiento;
+            if (puntajeAcumulado < 7)
+                reconocimiento = "diploma";
+            else if (puntajeAcumulado < 14)
+                reconocimiento = "Cena Doble";
+            else if (puntajeAcumulado < 31)
+                reconocimiento = "Pasaje turístico doble";
+            else
+                reconocimiento = "Paquete turístico integral";
+
+            return reconocimiento;
+        } 
+
+
+        static void Incrementos(int tipo, int cantidad)
+        {
+            switch (tipo)
+            {
+                case 1: //Oro
+                    TotalMedallasOro += cantidad;
+                    canDeportistasOro++;
+                    break;
+                case 2: //Plata
+                    TotalMedallasPlata += cantidad;
+                    canDeportistasPlata++;
+                    break;
+                case 3: //Bronce
+                    TotalMedallasBronce += cantidad;
+                    canDeportistasBronce++;
+                    break;
+                default:    //Mención Honrosa
+                    TotalMedallasMencionHonrosa +=  cantidad;
+                    canDeportistasMencionHonrosa ++;
+                    break;
+            }
+        }
+    }
+}
---
+++ SebastiánEisnerSalasCueva/Pregunta02_SebastiánSalas/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta02_SebastiánSalas
+{
+    internal class Program
+    {
+        static int ventasAcademico = 0, ventasNegocios = 0, ventasPlus = 0;
+        static int clientesAcademico = 0, clientesNegocios = 0, clientesPlus = 0;
+        static double totalAcademico = 0, totalNegocios = 0, totalPlus = 0;
+        static void Main(string[] args)
+        {
+
+            int tipo, cantidad;
+            double importedecompra, importededescuento, importeFinal, totales;
+
+            do
+            {
+
+                Console.WriteLine("Tipo de licencias: 1.Académico 2.Negocios 3.Plus 4.Salir");
+                tipo = int.Parse(Console.ReadLine());
+
+                if (tipo == 4)
+                {
+                    Console.WriteLine("Saliendo del programa...");
+                    break;
+                }
+
+                Console.WriteLine("Cantidad: ");
+                cantidad = int.Parse(Console.ReadLine());
+
+                importedecompra = importeCompra(tipo, cantidad);
+                Console.WriteLine("El importe de compra es: " + importedecompra);
+                importededescuento = importeDescuento(importedecompra, cantidad);
+                Console.WriteLine("El descuento del importe es: " + importededescuento);
+                importeFinal = importePagar(importedecompra, importededescuento);
+                Console.WriteLine("El importe de pagar es: " + importeFinal);
+                Totales(tipo, cantidad, importeFinal);
+                ImprimirResultado();
+
+
+
+            } while (tipo!=4);
+
+            
+
+        }
+
+        static void ImprimirResultado() 
+        {
+            Console.WriteLine("Cantidad total de licencias Académico: " + ventasAcademico);
+            Console.WriteLine("Cantidad total de licencias Negocios: " + ventasNegocios);
+            Console.WriteLine("Cantidad total de licencias Plus: " + ventasPlus);
+
+            Console.WriteLine("Clientes con licencias Académico: " + clientesAcademico);
+            Console.WriteLine("Clientes con licencias Negocios: " + clientesNegocios);
+            Console.WriteLine("Clientes con licencias Plus: " + clientesPlus);
+
+            Console.WriteLine("Total acumulado Académico: S/. " + totalAcademico);
+            Console.WriteLine("Total acumulado Negocios: S/. " + totalNegocios);
+            Console.WriteLine("Total acumulado Plus: S/. " + totalPlus);
+
+        }
+
+        static double importeCompra(int tipo, int cantidad)
+        {
+            double precio=0;
+            
+            switch (tipo) 
+            {
+                case 1:
+                precio = 45;
+                    break;
+                case 2:
+                precio = 75;
+                    break;
+                case 3:
+                precio = 100;
+                    break;
+                default:
+                break;
+            
+            }
+            return precio * cantidad;
+        }
+
+        static double importeDescuento(double importe, int cantidad)
+        {
+            double descuento =0;
+
+            if (cantidad < 4)
+                descuento = 0.045;
+            else if (cantidad >= 4 && cantidad < 12)
+                descuento = 0.07;
+            else if (cantidad >= 12)
+                descuento = 0.105;
+
+            return importe*descuento;
+        }
+
+        static double importePagar(double importeCompra, double importeDescuento) 
+        {
+            
+            return importeCompra - importeDescuento;
+        }
+
+        static void Totales(int tipo, int cantidad, double importeFinal)
+        {
+            switch (tipo)
+            {
+                case 1:
+                    ventasAcademico += cantidad;
+                    clientesAcademico++;
+                    totalAcademico += importeFinal;
+                    break;
+                case 2:
+                    ventasNegocios += cantidad;
+                    clientesNegocios++;
+                    totalNegocios += importeFinal;
+                    break;
+                case 3: 
+                    ventasPlus += cantidad;
+                    clientesPlus++;
+                    totalPlus += importeFinal;
+                    break;
+            }
+              
+        }
+    }
+}
